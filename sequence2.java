5704580d60886746a6755e2c43e8a7da2ad6e56a
anfisakryvtsun <113179772+anfisakryvtsun@users.noreply.github.com>
2022-10-06 23:32:08 +0200

    Create sequence2.java

---
+++ sequence2.java
@@ -0,0 +1,7 @@
+public class sequence2 {
+    public static void main(String[] args) throws Exception {
+        for (int i = 1000; i >=1; i--) {
+            System.out.println(i);
+          }
+}
+}
